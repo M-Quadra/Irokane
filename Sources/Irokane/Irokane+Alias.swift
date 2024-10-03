2cacfa61e6a30c3aaa621d35b42260dc953be307
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2024-10-03 18:44:17 +0800

    feat: unsqueeze alias

--- Sources/Irokane/Irokane+Alias.swift
+++ Sources/Irokane/Irokane+Alias.swift
@@ -8,3 +8,7 @@
 public func clampMin(_ input: borrowing Graph.Tensor, _ min: Double) -> Graph.Tensor {
     return Irokane.maximum(input, min)
 }
+
+public func unsqueeze(_ input: borrowing Graph.Tensor, dim: Int) -> Graph.Tensor {
+    return input.unsqueeze(dim)
+}
