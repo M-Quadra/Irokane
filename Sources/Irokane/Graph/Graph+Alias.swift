f756a2c645b48ee7520db83a56b8239fdeafbf13
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2024-10-04 10:29:00 +0800

    feat: view

--- Sources/Irokane/Graph/Graph+Alias.swift
+++ Sources/Irokane/Graph/Graph+Alias.swift
@@ -5,6 +5,8 @@
 //  Created by m_quadra on 2024/10/2.
 //
 
+import Foundation
+
 public extension Graph.Tensor {
     
     borrowing func sum(_ dim: Int) -> Graph.Tensor {
@@ -14,5 +16,9 @@ public extension Graph.Tensor {
     /// int64
     borrowing func long() -> Graph.Tensor {
         return self.cast(to: .int64)
+    }
+    
+    borrowing func view(_ size: [NSNumber]) -> Graph.Tensor {
+        return self.reshape(size)
     }
 }
