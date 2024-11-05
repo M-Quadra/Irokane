54bd686e6c2fdf59ab4d0ad7b8801e7bc85361ba
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2024-11-06 07:27:09 +0800

    feat: Conv1d args

--- Sources/Irokane/Convolution.swift
+++ Sources/Irokane/Convolution.swift
@@ -6,42 +6,67 @@
 //
 
 import MetalPerformanceShadersGraph
+fileprivate typealias F = Irokane.Functional
 
 @available(iOS 15.4, *)
 public struct Conv1d {
     
     let weight: MPSGraphTensor
     let bias: MPSGraphTensor
+    let groups: Int
+    let padding: Int
     
-    init(weight: MPSGraphTensor, bias: MPSGraphTensor) {
+    init(
+        weight: MPSGraphTensor, // [out, in, kernel]
+        bias: MPSGraphTensor, // [out]
+        groups: Int,
+        padding: Int
+    ) {
         let mpsGraph = weight.operation.graph
         self.weight = mpsGraph.expandDims(consume weight, axis: -1, name: nil)
         self.bias = mpsGraph.expandDims(consume bias, axis: -1, name: nil)
+        self.groups = groups
+        self.padding = padding
     }
 }
 
 @available(iOS 15.4, *)
 public extension Conv1d {
     
-    init(weight: borrowing Graph.Tensor, bias: borrowing Graph.Tensor) {
-        self.init(weight: weight.tensor, bias: bias.tensor)
+    init(weight: borrowing Graph.Tensor, bias: borrowing Graph.Tensor, groups: Int = 1, padding: Int = 0) {
+        self.init(weight: weight.tensor, bias: bias.tensor, groups: groups, padding: padding)
+    }
+    
+    var outChannels: Int? {
+        self.weight.shape?.first?.intValue
+    }
+    var inChannels: Int? {
+        guard let shape = self.weight.shape, shape.count > 1 else { return nil }
+        return shape[1].intValue * self.groups
+    }
+    var kernelSize: Int? {
+        guard let shape = self.weight.shape, shape.count > 2 else { return nil }
+        return shape[2].intValue
     }
     
     func forward(_ input: borrowing Graph.Tensor) throws -> Graph.Tensor {
         guard let des = MPSGraphConvolution2DOpDescriptor(
             strideInX: 1, strideInY: 1,
             dilationRateInX: 1, dilationRateInY: 1,
-            groups: 1,
+            groups: self.groups,
             paddingStyle: .explicit,
             dataLayout: .NCHW,
             weightsLayout: .OIHW
         ) else { throw Errors.msg("MPSGraphConvolution2DOpDescriptor") }
         let graph = input.graph, mpsGraph = graph.mpsGraph
-        let x = mpsGraph.expandDims(input.tensor, axis: -1, name: nil)
+        let xp = if self.padding > 0 {
+            F.pad(input, pad: (left: self.padding, right: self.padding)).tensor
+        } else { input.tensor }
+        let x = mpsGraph.expandDims(xp, axis: -1, name: nil)
         
         let x0 = mpsGraph.convolution2D(x, weights: self.weight, descriptor: consume des, name: nil)
-        let x1 = mpsGraph.addition(consume x0, self.bias, name: nil)
-        let y = mpsGraph.squeeze(consume x1, axis: -1, name: nil)
+        let x1 = mpsGraph.squeeze(consume x0, axis: -1, name: nil)
+        let y = mpsGraph.addition(consume x1, self.bias, name: nil)
         return graph.tensor(consume y)
     }
 }
--- Tests/IrokaneTests/ConvolutionTests.swift
+++ Tests/IrokaneTests/ConvolutionTests.swift
@@ -12,14 +12,16 @@ import CoreML
 struct ConvolutionTests {
     
     @available(iOS 15.4, *)
-    @Test func conv1d() throws {
+    @Test("Conv1d(3, 2)")
+    func conv1d0() throws {
         let graph = Irokane.Graph()
         let w = try MLMultiArray(0..<6).ik.to(graph: graph)
             .cast(to: .float32)
             .reshape([2, 3, 1])
         let b = try MLMultiArray([Float32](repeating: 0.5, count: 2)).ik.to(graph: graph)
-        
         let conv1d = Conv1d(weight: w, bias: b)
+        #expect(conv1d.inChannels == 3)
+        #expect(conv1d.outChannels == 2)
         
         let x = try MLMultiArray(0..<6).ik.to(graph: graph)
             .cast(to: .float32)
@@ -33,6 +35,74 @@ struct ConvolutionTests {
         #expect(arr == [
             10.5, 13.5,
             28.5, 40.5,
+        ])
+    }
+    
+    @available(iOS 15.4, *)
+    @Test("Conv1d(3, 3)")
+    func conv1d1() throws {
+        let graph = Irokane.Graph()
+        let w = try MLMultiArray(shape: [3, 3, 1], dataType: .float32)
+            .ik.to(graph: graph)
+        let b = try MLMultiArray(shape: [3], dataType: .float32)
+            .ik.to(graph: graph)
+        let conv1d = Conv1d(weight: w, bias: b)
+        #expect(conv1d.inChannels == 3)
+        #expect(conv1d.outChannels == 3)
+        
+        let x = try MLMultiArray(shape: [1, 3, 5], dataType: .float32)
+            .ik.to(graph: graph)
+        let y = try conv1d.forward(x)
+        
+        let yData = try y.tensorData()
+        #expect(yData.shape == [1, 3, 5])
+    }
+    
+    @available(iOS 15.4, *)
+    @Test("Conv1d(5, 5, kernel_size=3, groups=5)")
+    func conv1d2() throws {
+        let graph = Irokane.Graph()
+        let w = graph.arange(end: 15, dtype: .float32)
+            .reshape([5, 1, 3])
+        let b = graph.zeros(5).cast(to: .float32) + 0.5
+        let conv1d = Conv1d(weight: w, bias: b, groups: 5)
+        #expect(conv1d.inChannels == 5)
+        #expect(conv1d.outChannels == 5)
+        
+        let x = graph.arange(end: 15, dtype: .float32)
+            .reshape([1, 5, 3])
+        let y = try conv1d.forward(x)
+        
+        let yData = try y.tensorData()
+        let arr = try yData.ik.toFloat32s()
+        #expect(yData.shape == [1, 5, 1])
+        #expect(arr == [5.5, 50.5, 149.5, 302.5, 509.5])
+    }
+    
+    @available(iOS 15.4, *)
+    @Test("Conv1d(5, 5, kernel_size=3, groups=5, padding=1)")
+    func conv1d3() throws {
+        let graph = Irokane.Graph()
+        let w = graph.arange(end: 15, dtype: .float32)
+            .reshape([5, 1, 3])
+        let b = graph.zeros(5).cast(to: .float32) + 0.5
+        let conv1d = Conv1d(weight: w, bias: b, groups: 5, padding: 1)
+        #expect(conv1d.inChannels == 5)
+        #expect(conv1d.outChannels == 5)
+        
+        let x = graph.arange(end: 15, dtype: .float32)
+            .reshape([1, 5, 3])
+        let y = try conv1d.forward(x)
+        
+        let yData = try y.tensorData()
+        let arr = try yData.ik.toFloat32s()
+        #expect(yData.shape == [1, 5, 3])
+        #expect(arr == [
+              2.5,   5.5,   2.5,
+             32.5,  50.5,  32.5,
+             98.5, 149.5,  98.5,
+            200.5, 302.5, 200.5,
+            338.5, 509.5, 338.5,
         ])
     }
 }
